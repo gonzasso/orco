217a20dba482110567b1911c205ec35804e1c080
Gonzalo Sassoni <gonzasso@gmail.com>
2017-04-04 21:10:57 -0300

    Add extraction of contours from video

--- BackgroundSubstraction.py
+++ BackgroundSubstraction.py
@@ -16,6 +16,7 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 fgbg = cv2.createBackgroundSubtractorMOG2(history=50, varThreshold=100, detectShadows=False)
 # fgbg = cv2.createBackgroundSubtractorKNN(history=10, dist2Threshold=200, detectShadows=False)
+count = 0
 
 while True:
     ret, frame = cap.read()
@@ -26,52 +27,55 @@ while True:
 
     fgmask = cv2.dilate(fgmask, None, iterations=2)
 
-    foreground_img = cv2.bitwise_and(frame, frame, mask = fgmask)
+    foreground_img = cv2.bitwise_and(frame, frame, mask=fgmask)
 
-    foundLocations, _ = hog.detectMultiScale(fgmask, 0, (8, 8), (32, 32), 1.05, 2)
+    # foundLocations, _ = hog.detectMultiScale(fgmask, 0, (8, 8), (32, 32), 1.05, 2)
 
     # find contours and hierarchy in frame
     _, cnts, hierarchy = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,
                                           cv2.CHAIN_APPROX_SIMPLE)
 
     # fill contours
-    # cv2.drawContours(fgmask, cnts, -1, (255, 255, 255), thickness=cv2.FILLED, hierarchy=hierarchy, maxLevel=2)
+    cv2.drawContours(fgmask, cnts, -1, (255, 255, 255), thickness=cv2.FILLED, hierarchy=hierarchy, maxLevel=2)
 
     # define params for blob detector
-    params = cv2.SimpleBlobDetector_Params()
-    params.filterByArea = True
-    params.minArea = 750
-    params.filterByConvexity = True
-    params.minConvexity = 0.1
-    params.filterByCircularity = True
-    params.minCircularity = 0.6
-    params.filterByColor = True
-    params.blobColor = 255
-    # create blob detector
-    detector = cv2.SimpleBlobDetector_create(params)
-
-    # find blobs in frame
-    blobs = detector.detect(fgmask)
-
-    im_with_keypoints = frame.copy()
-    # # loop over the contours
-    # for c in cnts:
-    #     # if the contour is too small, ignore it
-    #     if cv2.contourArea(c) < args["min_area"]:
-    #         continue
+    # params = cv2.SimpleBlobDetector_Params()
+    # params.filterByArea = True
+    # params.minArea = 750
+    # params.filterByConvexity = True
+    # params.minConvexity = 0.1
+    # params.filterByCircularity = True
+    # params.minCircularity = 0.6
+    # params.filterByColor = True
+    # params.blobColor = 255
+    # # create blob detector
+    # detector = cv2.SimpleBlobDetector_create(params)
     #
-    #     # compute the bounding box for the contour, draw it on the frame,
-    #     # and update the text
-    #     (x, y, w, h) = cv2.boundingRect(c)
-    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-    #     # draw circles for the blobs found
-    #     # im_with_keypoints = cv2.drawKeypoints(frame, blobs, np.array([]), (0, 0, 255),
-    #     #                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    #     # cv2.imshow('frame', im_with_keypoints)
+    # # find blobs in frame
+    # blobs = detector.detect(fgmask)
+    #
+    # im_with_keypoints = frame.copy()
+    # # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
 
-    for person in foundLocations:
-        (x, y, w, h) = person
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        crop_img = frame[y:y+h, x:x+w]
+        cv2.imwrite(".\img\/contour%d.jpg" % count, crop_img)
+        count += 1
+        # draw circles for the blobs found
+        # im_with_keypoints = cv2.drawKeypoints(frame, blobs, np.array([]), (0, 0, 255),
+        #                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # cv2.imshow('frame', im_with_keypoints)
+
+    # for person in foundLocations:
+    #     (x, y, w, h) = person
+    #     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     cv2.imshow('foreground', foreground_img)
     cv2.imshow('frame', frame)
